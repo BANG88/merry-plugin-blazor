eaae5417f66330d786993595f3911f344f355780
bang <sqibang@gmail.com>
2019-06-26 10:33:22 +0800

    feat: generate sub components

--- index.ts
+++ index.ts
@@ -6,68 +6,99 @@ import path from 'path'
 export interface BlazorAnswers {
   name: string
 }
+export interface BlazorOptions {
+  /** generate subcomponent */
+  sub?: string
+}
 export default (api: Plugin) => {
-  api.command('blazor [name]').action(async (name: string) => {
-    // define your own questions or remove it if you don't need it
-    const answers = name
-      ? {
-          name,
-        }
-      : await api.prompt<BlazorAnswers>([
-          {
-            name: 'name',
-            message: 'Your lib?',
-            validate: blazorName => {
-              if (!blazorName) {
-                return 'blazor name are required'
-              }
-              return true
+  api
+    .command('blazor [name]')
+    .option('-s --sub [sub]', 'sub component')
+    .action(async (name: string, options: BlazorOptions) => {
+      // define your own questions or remove it if you don't need it
+      const answers = name
+        ? {
+            name,
+          }
+        : await api.prompt<BlazorAnswers>([
+            {
+              name: 'name',
+              message: 'Your lib?',
+              validate: blazorName => {
+                if (!blazorName) {
+                  return 'blazor name are required'
+                }
+                return true
+              },
+              default: name,
             },
-            default: name,
-          },
-        ])
-    const ns = 'AntDesign'
-    const prefix = 'Ant'
-    const componentName = await api.compile(
-      `${prefix}{{properCase name}}Component`,
-      answers
-    )
-    const fullNamespace = await api.compile(`${ns}.{{properCase name}}`, answers)
+          ])
 
-    const data = {
-      ...answers,
-      componentName,
-      ns,
-      prefix,
-      fullNamespace,
-    }
+      await generateFiles(api, answers)
 
-    const proj = path.join(api.conf.dist, fullNamespace)
+      if (options.sub) {
+        const subComponents = options.sub.split(',')
+        for (const s of subComponents) {
+          await generateFiles(
+            api,
+            {
+              name: s.trim(),
+            },
+            answers.name
+          )
+        }
+      }
 
-    // empty folder
-    await api.fs.ensureDir(path.join(proj, 'content'))
+      const projs = await api.fs.readdir(api.conf.dist)
 
-    // razor
-    await api.tmpl(
-      'tpl/razor.tpl',
-      path.join(proj, `${prefix}{{properCase name}}.razor`),
-      data
-    )
-    // cs
-    await api.tmpl('tpl/cs.tpl', path.join(proj, `${componentName}.cs`), data)
+      api.log('Projects: %s', projs.join('\n'))
+    })
+}
 
-    // md
-    await api.tmpl('tpl/md.tpl', path.join(proj, `README.md`), data)
+async function generateFiles(
+  api: Plugin,
+  answers: { name: string },
+  namespace = ''
+) {
+  const ns = 'AntDesign'
+  const prefix = 'Ant'
+  const componentName = await api.compile(
+    `${prefix}{{properCase name}}Component`,
+    answers
+  )
+  const fullNamespace = await api.compile(
+    `${ns}.{{properCase name}}`,
+    namespace ? { name: namespace } : answers
+  )
+  const data = {
+    ...answers,
+    componentName,
+    ns,
+    prefix,
+    fullNamespace,
+  }
+  const proj = path.join(api.conf.dist, fullNamespace)
+  // empty folder
+  await api.fs.ensureDir(path.join(proj, 'content'))
+  // razor
+  await api.tmpl(
+    'tpl/razor.tpl',
+    path.join(proj, `${prefix}{{properCase name}}.razor`),
+    data
+  )
+  // cs
+  await api.tmpl('tpl/cs.tpl', path.join(proj, `${componentName}.cs`), data)
 
-    // csproj
-    await api.tmpl(
-      'tpl/csproj.tpl',
-      path.join(proj, `${fullNamespace}.csproj`),
-      data
-    )
-
-    const projs = await api.fs.readdir(api.conf.dist)
-
-    api.log('Projects: %s', projs.join('\n'))
-  })
+  // do not generate csproj files
+  if (namespace) {
+    return
+  }
+  // md
+  await api.tmpl('tpl/md.tpl', path.join(proj, `README.md`), data)
+  // csproj
+  await api.tmpl(
+    'tpl/csproj.tpl',
+    path.join(proj, `${fullNamespace}.csproj`),
+    data
+  )
 }
